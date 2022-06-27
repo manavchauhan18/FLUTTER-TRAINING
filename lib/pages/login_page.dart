4c69e18acccff57aa747edb53f4a838b5ecb9056
Manav Chauhan <manavchauhan277@gmail.com>
2022-06-27 14:01:02 +0530

    DAY 5

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/routes.dart';
 
 class LoginPage extends StatelessWidget{
   const LoginPage({Key? key}) : super(key: key);
@@ -9,48 +10,53 @@ class LoginPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child : Column(
-        children: [
-          Image.asset('assets/images/login.png', 
-          fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text("Welcome",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Enter UserName",
-                  labelText: "Username",
+      child : SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/login.png', 
+            fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text("Welcome",style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter UserName",
+                    labelText: "Username",
+                  ),
                 ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "Enter Password",
-                  labelText: "Password",
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              ElevatedButton(
-                style: TextButton.styleFrom(),
-                onPressed: () {
-                  print("HI!!!");
-                },
-                child: Text("Login"),
-                )
-            ],),
-          )
-        ],
+                const SizedBox(
+                  height: 40.0,
+                ),
+      
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    minimumSize: const Size(150, 40),
+                  ),
+                  onPressed: () {
+                    // ignore: avoid_print
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                  child: const Text("Login"),
+                  )
+              ],),
+            )
+          ],
+        ),
       ));
   }
 } 
